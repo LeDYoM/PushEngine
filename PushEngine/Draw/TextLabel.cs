8af30b028dbf43d5c445e32fbda9bb552df6c1c3
LeDYoM <ledyom@hotmail.com>
2013-06-20 21:33:54 +0200

    TextLabel changes

--- PushEngine/Draw/TextLabel.cs
+++ PushEngine/Draw/TextLabel.cs
@@ -6,6 +6,11 @@ namespace PushEngine.Draw
 {
     internal class TextLabel : Quad
     {
+        public string text = "none";
+        public Font font = SystemFonts.DefaultFont;
+        Color4 backgroundColor = Color4.Red;
+        Color4 foregroundColor = Color4.Blue;
+
         internal TextLabel(): base()
         {
         }
@@ -14,7 +19,7 @@ namespace PushEngine.Draw
         {
             // Create a new text texture.
 
-            texture = Texture.CreateTextTexture("test agromenauer", SystemFonts.DefaultFont, Color4.Blue, Color4.Red);
+            texture = Texture.CreateTextTexture(text, font, foregroundColor, backgroundColor);
             base.PostInit();
 
         }
