e8717b7254c6d217aab2b6a4a805293b93e32b0b
Ismael <ismael.gonzalez@scoreloop.com>
2014-02-04 17:04:31 +0100

    WIP in TokenGrpup component. Added some methods to manage it.

--- PushEngine/Demos/Zooper/TokenGroup.cs
+++ PushEngine/Demos/Zooper/TokenGroup.cs
@@ -2,6 +2,7 @@ using System;
 using PushEngine.Draw.Components;
 using PushEngine.Draw;
 using System.Collections.Generic;
+using System.Drawing;
 
 namespace PushEngine.Demos.Zooper
 {
@@ -9,8 +10,8 @@ namespace PushEngine.Demos.Zooper
 	{
 		private DynamicImageRenderer diRenderer;
 		private List<List<int>> model = new List<List<int>>();
-		private int x = 0;
-		private int y = 0;
+		private int width = 0;
+		private int height = 0;
 
 		public enum Direction
 		{
@@ -26,8 +27,8 @@ namespace PushEngine.Demos.Zooper
 		{
 			parent.GetNewDrawElement<DynamicImageRenderer> ("TokenGroup");
 
-			x = x_;
-			y = y_;
+			width = x_;
+			height = y_;
 			direction = direction_;
 
 			ResetModel ();
@@ -37,24 +38,115 @@ namespace PushEngine.Demos.Zooper
 		{
 			model.Clear ();
 
-			for (int x_ = x; x_ < x; ++x_) {
+			for (int x_ = width; x_ < width; ++x_) {
 				model.Add (new List<int> ());
-				for (int y_ = y; y_ < y; ++y_) {
-					model [x].Add (0);
+				for (int y_ = height; y_ < height; ++y_) {
+					model [width].Add (0);
 				}
 			}
 		}
 
-		public void setTokenInModel(int x, int y, int type_)
+		private bool isHorizontal { get { return direction == Direction.Left || direction == Direction.Right; } }
+		private bool isVertical { get { return !isHorizontal; } }
+
+		private void EndReached(Point entered)
 		{
-			model [x] [y] = type_;
 		}
 
-		public void moveModelToken(int xCurrent, int yCurrent)
+		public void setTokenInModel(Point p_, int type_)
 		{
-			int temp = model [x] [y];
+			model [p_.X] [p_.Y] = type_;
+		}
+
+		public void moveModelToken(Point origin_, Point dest_)
+		{
+			int temp = model [origin_.X] [origin_.Y];
+			PEDebug.Assert (model [origin_.X] [origin_.Y] != 0, "No valid type in source position: " + origin_);
+			PEDebug.Assert (model [dest_.X] [dest_.Y] == 0, "Destionation is not empty: " + dest_+": "+ model[dest_.X][dest_.Y]);
+
+			model [dest_.X] [dest_.Y] = model [origin_.X] [origin_.Y];
+			model [origin_.X] [origin_.Y] = 0;
+		}
+
+		private void AddToRight(int line, int type_)
+		{
+			setTokenInModel (new Point (width - 1, line), type_);
+
+			for (int x = 0; x <  width; ++x) {
+				if (model [x] [line] != 0) {
+					if (x == 0) {
+						EndReached (new Point (x, line));
+						model [x] [line] = 0;
+					}
+					moveModelToken(new Point(x, line), new Point(x - 1, line));
+				}
+			}
+		}
+
+		private void AddToLeft(int line, int type_)
+		{
+			setTokenInModel (new Point (0, line), type_);
+
+			for (int x = width - 1; x > -1 ; --x) {
+				if (model [x] [line] != 0) {
+					if (x == (width - 1)) {
+						EndReached (new Point (x, line));
+						model [x] [line] = 0;
+					}
+					moveModelToken(new Point(x, line), new Point(x + 1, line));
+				}
+			}
+		}
+
+
+		private void AddToBottom(int column, int type_)
+		{
+			setTokenInModel (new Point (column, height - 1), type_);
+
+			for (int y = 0; y <  height; ++y) {
+				if (model [column] [y] != 0) {
+					if (y == 0) {
+						EndReached (new Point (column, y));
+						model [column] [y] = 0;
+					}
+					moveModelToken(new Point(column, y), new Point(column, y - 1));
+				}
+			}
+		}
+
+		private void AddToTop(int column, int type_)
+		{
+			setTokenInModel (new Point (column, 0), type_);
+
+			for (int y = height - 1; y > -1; --y) {
+				if (model [column] [y] != 0) {
+					if (y == (height - 1)) {
+						EndReached (new Point (column, y));
+						model [column] [y] = 0;
+					}
+					moveModelToken(new Point(column, y), new Point(column, y - 1));
+				}
+			}
+		}
+
+		public void Add(int p, int type_)
+		{
+			switch (direction)
+			{
+				case Direction.Left:
+					AddToLeft (p, type_);
+					break;
+				case Direction.Right:
+					AddToRight (p, type_);
+					break;
+				case Direction.Up:
+					AddToBottom (p, type_);
+					break;
+				case Direction.Down:
+					AddToTop (p, type_);
+					break;
+			}
 
 		}
 	}
 }
-
